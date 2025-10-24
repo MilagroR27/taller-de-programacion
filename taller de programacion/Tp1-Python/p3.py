8e82fb89e63016ef76c8018e523508764bed9e5c
MilagroR27 <rodriguezmm05@gmail.com>
2025-10-24 00:26:28 -0300

    Actualice Carpets y Ejercicio 3

--- taller de programacion/Tp1-Python/p3.py
+++ taller de programacion/Tp1-Python/p3.py
@@ -1,1 +1,14 @@
 #Se ingresan N valores numéricos. Se desea saber cuántos son positivos, cuantos negativos y cuantos iguales a cero. 
+cont_p=0
+cont_n=0
+cont_c=0
+N=int(input("Ingresa la cantidad de numeros que desea colocar "))
+for i in range(N):
+    num=int(input("Ingrese los numeros "))
+    if num == 0:
+        cont_c +=1
+    elif num > 0:
+        cont_p +=1
+    else:
+        cont_n +=1
+
